ddc512a2df7e95b835a2d16f24d69c655c044ed8
Edson Y. Toma <edson.yudi.toma@gmail.com>
2025-05-16 17:10:59 -0300

    feat: Initial setup (#1)
    * Add base template
    
    * Add base config files
    
    * Add fastlane
    
    * Add github actions
    
    * Update readme
    
    * Add changelog
    
    * Add lint helper
    
    * Remove forced test fails
    
    * Add forgotten setup ci
    
    * Force fail to test
    
    * Bump xcode version
    
    * Remove force fail
    
    * Disable tests (too expensive haha)
    
    * Add if to allow upload when tests fail

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# gem "rails"
+gem 'fastlane', '2.227.2'
+gem 'xcodeproj', '1.27'
+gem 'yaml', '0.2.1'
---
+++ MoviesSampleApp/MoviesSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesSampleApp.swift
+//  MoviesSampleApp
+//
+//  Created on 05/15/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
