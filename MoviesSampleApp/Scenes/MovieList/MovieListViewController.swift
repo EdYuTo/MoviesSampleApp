0eabffc303709576869adf5adb566501061c47e1
EdYuTo <edson.yudi.toma@gmail.com>
2025-05-18 21:34:49 -0300

    Add helper for alert view

--- MoviesSampleApp/Scenes/MovieList/MovieListViewController.swift
+++ MoviesSampleApp/Scenes/MovieList/MovieListViewController.swift
@@ -157,45 +157,26 @@ extension MovieListViewController: MovieListViewProtocol {
 
     func displayError() {
         if dataSource.count == 0 {
-            displayError(
+            let alert = makeAlertView(
                 title: Localizable.errorAlertTitle.localized,
                 buttonTitle: Localizable.retryButtonTitle.localized
             ) { [weak self] in
                 self?.interactor.fetchData()
             }
+            router.present(alert)
         } else {
             replaceLastState(with: .error)
         }
     }
 
     func displayInternetError() {
-        displayError(
+        let alert = makeAlertView(
             title: Localizable.internetErrorTitle.localized,
             description: Localizable.internetErrorMessage.localized,
             buttonTitle: Localizable.retryButtonTitle.localized
         ) { [weak self] in
             self?.interactor.fetchData()
         }
-    }
-
-    private func displayError(
-        title: String,
-        description: String? = nil,
-        buttonTitle: String,
-        action: (() -> Void)?
-    ) {
-        let alert = UIAlertController(
-            title: title,
-            message: description,
-            preferredStyle: .alert
-        )
-        let action = UIAlertAction(
-            title: buttonTitle,
-            style: .default
-        ) { _ in
-            action?()
-        }
-        alert.addAction(action)
         router.present(alert)
     }
 
